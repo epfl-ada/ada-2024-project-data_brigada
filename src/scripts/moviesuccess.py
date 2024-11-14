15e01fc3d5d194ce12c3732f9e878a1a2f76ff57
Arian Bislimi <ari@tsf-428-wpa-1-083.epfl.ch>
2024-11-14 12:48:57 +0100

    results.ipynb subquestions

--- src/scripts/moviesuccess.py
+++ src/scripts/moviesuccess.py
@@ -4,7 +4,7 @@ import os
 
 # Define the paths
 MOVIE_METADATA_PATH = "../../data/MovieSummaries/movie.metadata.tsv"
-OUTPUT_TOP_5_PERCENT_PATH = os.path.join("..", "..", "data", "MovieSummaries", "top_2_percent_movies.tsv")
+OUTPUT_TOP_5_PERCENT_PATH = os.path.join("..", "..", "data", "MovieSummaries", "top_5_percent_movies.tsv")
 
 # Function to load the movie metadata file
 def load_movie_metadata(file_path):
@@ -20,7 +20,7 @@ def get_top_5_percent_successful_movies(movies_df):
     # Drop rows where "Box office revenue" is NaN
     movies_df = movies_df.dropna(subset=["Box office revenue"])
     # Calculate the threshold for the top 5% most successful movies
-    top_5_percent_threshold = movies_df["Box office revenue"].quantile(0.98)
+    top_5_percent_threshold = movies_df["Box office revenue"].quantile(0.95)
     # Filter the movies to include only those in the top 5% by box office revenue
     top_5_percent_movies_df = movies_df[movies_df["Box office revenue"] >= top_5_percent_threshold]
     return top_5_percent_movies_df
